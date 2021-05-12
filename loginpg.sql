eef65a5535392213e16d674c865f6eb7de47e8a6
pdvmoto <piet@basetide.com>
2021-05-12 12:32:37 +0200

    prompting

---
+++ loginpg.sql
@@ -0,0 +1,13 @@
+-- set prompt  to know "where we are"
+\set PROMPT1 '%n @ %/%R > '
+
+-- \set PROMPT1 '%M %n @ %/%R%[%033[0m%]%# '
+
+-- optinally: inet_server_addr()
+
+select 'welcome to psql' ; 
+
+\echo  your dflt directory is ; \! pwd 
+
+\echo .
+
---
+++ psqlrc.sql
@@ -0,0 +1,14 @@
+
+-- use this if a .psqlrc is allowed/needed
+
+-- set prompt  to know "where we are"
+\set PROMPT1 '%n @ %/%R > '
+
+-- optinally: inet_server_addr()
+
+select 'welcome to psql' ; 
+
+\echo  your dflt directiry is ; \! pwd 
+
+\echo .
+
