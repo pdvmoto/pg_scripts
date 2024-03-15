834c5ac8f139cbeeef930cfa2ca25d642eb20dc4
pdvbv macbook <piet.devisser@cheerful.com>
2024-03-15 17:14:42 +0100

    tabluid

--- yb_init.sql
+++ yb_init.sql
@@ -21,10 +21,22 @@ alter database yugabyte set yb_enable_base_scans_cost_model to true;
 
 /* */ 
 
+\echo function from jim nicely to get tableid
+CREATE OR REPLACE FUNCTION get_table_id(schema_name_p VARCHAR, table_name_p VARCHAR) RETURNS VARCHAR
+AS $$
+SELECT '0000' || lpad(to_hex(d.oid::int), 4, '0') || '00003000800000000000' || lpad(to_hex(c.oid::int), 4, '0') tableid
+  FROM pg_class c, pg_namespace n, pg_database d
+ WHERE n.nspname = $1
+   AND c.relname = $2
+   AND c.relnamespace = n.oid
+   AND d.datname=current_database();
+$$ LANGUAGE SQL;
+
 \echo view to inspect yb-properties of tables
 create or replace view ybx_tblinfo as (
 select c.oid
 , n.nspname as schemaname
+, c.relkind
 , c.relname
 , pg_catalog.pg_get_userbyid(c.relowner) as tableowner
 , tp.num_tablets
@@ -33,11 +45,15 @@ select c.oid
 , tp.tablegroup_oid
 , tp.colocation_id
 , yb_is_local_table ( c.oid) islocal
---, c.*
-from   pg_catalog.pg_class c 
-left join pg_catalog.pg_namespace n on n.oid = c.relnamespace 
+, case when n.nspname not in ( 'pg_catalog', 'information_schema')
+       then pg_table_size(c.oid )
+       else null
+  end as size_bytes
+from   pg_catalog.pg_class c
+left join pg_catalog.pg_namespace n on n.oid = c.relnamespace
 join  yb_table_properties(c.oid) tp on 1=1
-where c.relkind in ('r', 'i', 't', 'S','m') )
+where c.relkind in ('r', 'i', 't', 'm', 'S') 
+)
 ;
 
 
