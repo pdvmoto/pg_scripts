860c5b77759aa9d393c347171bd80a2411d56237
pdvbv macbook <piet.devisser@cheerful.com>
2024-06-27 09:44:12 +0200

    omre

--- do_fill.sql
+++ do_fill.sql
@@ -1,5 +1,5 @@
 
-select f_fill_t (1000), f_fill_t_rndm (1000) , now () ; 
+select f_fill_t (100), f_fill_t_rndm (100) , now () ; 
 
 \watch 1
 
--- yb_ash.sql
+++ yb_ash.sql
@@ -2,7 +2,7 @@
 
 select h.sample_time, h.root_request_id, h.rpc_request_id, h.wait_event_component, h.query_id, h.ysql_session_id  , s.query
 from yb_active_session_history h 
-, pg_stat_statements s
+   , pg_stat_statements s
 where h.query_id = s.queryid
 and sample_time >= now()-interval ' 11600, second'
 -- and query like '%count%'
